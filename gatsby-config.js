7d96ada7b209ff41bdfd7f45b8d30cd4783b7bb2
KRBailey-dev <61134561+KRBailey-dev@users.noreply.github.com>
2020-02-17 02:56:31 -0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata:{
+    title:"BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@johndoe",
+    data: {
+        name: "john",
+        age: 24,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+  ],
+}
